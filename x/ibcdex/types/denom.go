9ec887e146b8995b34f9070da7566c22e549ee07
ltacker <lucas.bertrand.22@gmail.com>
2021-03-09 21:15:02 +0100

    Denom improvement

--- x/ibcdex/types/denom.go
+++ x/ibcdex/types/denom.go
@@ -2,6 +2,7 @@ package types
 
 import ibctransfertypes "github.com/cosmos/cosmos-sdk/x/ibc/applications/transfer/types"
 
+// VoucherDenom returns the voucher of the denom from the port ID and channel ID
 func VoucherDenom(port string, channel string, denom string) string {
 	// since SendPacket did not prefix the denomination, we must prefix denomination here
 	sourcePrefix := ibctransfertypes.GetDenomPrefix(port, channel)
@@ -13,4 +14,30 @@ func VoucherDenom(port string, channel string, denom string) string {
 	denomTrace := ibctransfertypes.ParseDenomTrace(prefixedDenom)
 
 	return denomTrace.IBCDenom()
+}
+
+// OriginalDenom returns back the original denom of the voucher
+// False is returned if the port ID and channel ID provided are not the origins of the voucher
+func OriginalDenom(port string, channel string, voucher string) (string, bool) {
+	// Check if original chain
+	if ibctransfertypes.ReceiverChainIsSource(port, channel, voucher) {
+		// remove prefix added by sender chain
+		voucherPrefix := ibctransfertypes.GetDenomPrefix(port, channel)
+		unprefixedDenom := voucher[len(voucherPrefix):]
+
+		// coin denomination used in sending from the escrow address
+		denom := unprefixedDenom
+
+		// The denomination used to send the coins is either the native denom or the hash of the path
+		// if the denomination is not native.
+		denomTrace := ibctransfertypes.ParseDenomTrace(unprefixedDenom)
+		if denomTrace.Path != "" {
+			denom = denomTrace.IBCDenom()
+		}
+
+		return denom, true
+	}
+
+	// Not the original chain
+	return "", false
 }
--- x/ibcdex/types/denom_test.go
+++ x/ibcdex/types/denom_test.go
@@ -1,1 +1,27 @@
-package types
+package types_test
+
+import (
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/interchange/x/ibcdex/types"
+	"testing"
+)
+
+func TestOriginalDenom(t *testing.T) {
+	port, channel, denom := "port-0", "channel-0", "foo"
+	port2, channel2, denom2 := "port-1", "channel-1", "bar"
+	voucher1 := types.VoucherDenom(port, channel, denom)
+	voucher2 := types.VoucherDenom(port2, channel2, denom)
+	voucher3 := types.VoucherDenom(port, channel, denom2)
+
+	// VoucherDenom generates different denoms
+	require.NotEqual(t, voucher1, voucher2)
+	require.NotEqual(t, voucher1, voucher3)
+
+	// OriginalDenom gets back the original denom
+	_, isOrigin := types.OriginalDenom(port2, channel2, voucher1)
+	require.False(t, isOrigin)
+
+	origin, isOrigin := types.OriginalDenom(port, channel, voucher1)
+	require.True(t, isOrigin)
+	require.Equal(t, denom, origin)
+}
